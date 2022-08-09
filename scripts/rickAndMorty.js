e4dd17baee2530ca8a13113ab677c10c2195cec8
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-08-09 16:33:51 -0300

    cards de rick and morty pintadas

--- scripts/rickAndMorty.js
+++ scripts/rickAndMorty.js
@@ -1,30 +1,26 @@
-var url ="https://rickandmortyapi.com/api/character"
-const selector = document.querySelector('.mainContainer_character')
+var url = "https://rickandmortyapi.com/api/character";
+const selector = document.querySelector(".mainContainer_character");
 
-const getData = async()=>{
-    try {
-        const data =  await fetch(url)
-        const res = await data.json() 
-       showmeData(res)
-    } catch (error) {
-        console.log(error)
-    }
+const getData = async () => {
+  try {
+    const data = await fetch(url);
+    const res = await data.json();
+    showmeData(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData();
+
+const showmeData = (data) => {
   
-
-}
-getData()
-
-const showmeData=(data)=>{
-    console.log(data)
-let body = ``
-data.results.map(item=>
-    
-      body+=`
+  let body = ``;
+  data.results.map(
+    (item) =>
+      (body += `
      <img src =${item.image}>
      <h1>${item.name}</h1>
-     <h3>${item.status}</h3>`
-     
-     
-)
-selector.innerHTML=body
-}
+     <h3>${item.status}</h3>`)
+  );
+  selector.innerHTML = body;
+};
